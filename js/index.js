9a02ef2bd3fd1cc946bd787a4597dfdc66394853
Małgosia Krzemińska <malgosia.krzeminska@yahoo.pl>
2017-03-15 15:30:32 +0100

    First commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function rysujChoinke(n) {
+    
+    for (var i = 1; i <= n; i++) {
+       var star =''; 
+    
+        for (var m = 1; m <= (i*2-1); m++) {
+             star += '*';
+        }
+        console.log(star);
+    }
+}
+   rysujChoinke(7)
